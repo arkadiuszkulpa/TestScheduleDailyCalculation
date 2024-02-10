d718fecba5076a1063164539cd5fcb7a5eecf271
Arkadiusz Kulpa <arkadiusz.kulpa.91@gmail.com>
2024-02-10 12:53:05 +0000

    shortened code for defining filepath

--- main.py
+++ main.py
@@ -2,52 +2,17 @@ import util
 import os
 
 from tkinter import filedialog, messagebox, Toplevel
-
 import pandas as pd
 import tkinter as tk
 
 from application import Application
-
 from analyzer import Analyzer
 
-#Check if running Executable or Python Script
-application_path = util.CheckExecutable()
-
-# Build the full path to the GIF file
-gif_path = os.path.join(application_path, 'math.gif')
-
 # Create an instance of Application
-app = Application(gif_path)
+app = Application(os.path.join(util.CheckExecutable(), 'math.gif'))
 
 # Open a file dialog for the user to select a file
 file_paths = app.open_files()
-
-def process_outcomes():
-    """calculate daily outcomes for each day and save a sum of outcomes for each day separately"""
-    # Create an instance of tc_dict
-    tc_dict_instance = analyzer.tc_dict.copy()
-
-    for index, row in history_worksheet.iterrows():
-        date = row['Date']
-        tc_id = row['TestCaseID']
-        outcome = row['Outcome']
-
-        # Update the tc_dict instance
-        tc_dict_instance[tc_id] = outcome
-
-        # Replace the tc_dict in date_tc_outcome_dict with the updated tc_dict instance
-        analyzer.date_tc_outcome_dict[date] = tc_dict_instance.copy()
-
-    #output an outcome count table
-    result_dict = {}
-
-    for date in analyzer.date_tc_outcome_dict:
-        result_dict[date] = {outcome: 0 for outcome in analyzer.outcome_set}
-
-        for tc_id, outcome in analyzer.date_tc_outcome_dict[date].items():
-            if outcome in analyzer.outcome_set:
-                result_dict[date][outcome] += 1
-    return result_dict
 
 for file_path in app.tk.splitlist(file_paths):
 
